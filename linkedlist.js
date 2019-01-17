b392a0d5ff08f36b9fa385794f29beca7a2cc435
RP Boyle <rpboyle11@yahoo.com>
2019-01-17 14:31:35 -0500

    Supplemental functions for linked list completed

--- linkedlist.js
+++ linkedlist.js
@@ -1,3 +1,5 @@
+'use strict';
+
 class _Node {
   constructor(value, next) {
     this.value = value;
@@ -26,47 +28,47 @@ class LinkedList {
     }
   }
 
-//   insertBefore(item, ptr){
-//     let currNode = this.head;
-//     let prevNode = this.head;
-//     while(currNode.next !== ptr){
-//         prevNode = currNode;
-//         currNode = currNode.next;
-//     }
-//     // currNode.next = new _Node(item, ptr);
-//     prevNode.next = new _Node(item,ptr);
-//   }
+  //   insertBefore(item, ptr){
+  //     let currNode = this.head;
+  //     let prevNode = this.head;
+  //     while(currNode.next !== ptr){
+  //         prevNode = currNode;
+  //         currNode = currNode.next;
+  //     }
+  //     // currNode.next = new _Node(item, ptr);
+  //     prevNode.next = new _Node(item,ptr);
+  //   }
 
-  insertBefore(newItem, targetItem){
+  insertBefore(newItem, targetItem) {
     // similar stuff
     let tempNode = this.find(targetItem);
     let currNode = this.head;
     let prevNode = this.head;
-    while(currNode.next !== tempNode){
-        prevNode = currNode;
-        currNode = currNode.next;
+    while (currNode.next !== tempNode) {
+      prevNode = currNode;
+      currNode = currNode.next;
     }
     // currNode.next = new _Node(item, ptr);
-    currNode.next = new _Node(newItem,tempNode);
+    currNode.next = new _Node(newItem, tempNode);
   }
 
-  insertAfter(newItem,targetItem){
+  insertAfter(newItem, targetItem) {
     let tempNode = this.find(targetItem);
     let currNode = this.head;
-    while(currNode !== tempNode){
-        currNode = currNode.next;
+    while (currNode !== tempNode) {
+      currNode = currNode.next;
     }
     currNode.next = new _Node(newItem, tempNode.next);
   }
 
-  insertAt(newItem,index){
+  insertAt(newItem, index) {
     let count = 0;
     let currNode = this.head;
-    while( count !== index){
-        currNode = currNode.next;
-        count++;
+    while (count !== index) {
+      currNode = currNode.next;
+      count++;
     }
-    this.insertBefore(newItem, currNode.value)
+    this.insertBefore(newItem, currNode.value);
   }
 
   find(item) {
@@ -90,15 +92,15 @@ class LinkedList {
     //found it
     return currNode;
   }
-  remove(item){ 
+  remove(item) {
     //if the list is empty
-    if (!this.head){
-        return null;
+    if (!this.head) {
+      return null;
     }
     //if the node to be removed is head, make the next node head
-    if(this.head.value === item){
-        this.head = this.head.next;
-        return;
+    if (this.head.value === item) {
+      this.head = this.head.next;
+      return;
     }
     //start at the head
     let currNode = this.head;
@@ -106,24 +108,22 @@ class LinkedList {
     let previousNode = this.head;
 
     while ((currNode !== null) && (currNode.value !== item)) {
-        //save the previous node 
-        previousNode = currNode;
-        currNode = currNode.next;
+      //save the previous node 
+      previousNode = currNode;
+      currNode = currNode.next;
     }
-    if(currNode === null){
-        console.log('Item not found');
-        return;
+    if (currNode === null) {
+      console.log('Item not found');
+      return;
     }
     previousNode.next = currNode.next;
-}
-
-
-
+  }
 }
 
 
 
 const newList = new LinkedList();
+const testList = new LinkedList();
 
 newList.insertLast('Apollo');
 newList.insertLast('Boomer');
@@ -131,11 +131,68 @@ newList.insertLast('Helo');
 newList.insertLast('Husker');
 newList.insertLast('Starbuck');
 newList.insertLast('Tauhida');
-newList.remove('squirrel');
+// newList.remove('squirrel');
 newList.insertAt('michael', 3);
-console.log(newList.find('Helo'));
+// console.log(newList.find('Helo'));
 
 // console.log(newList);
 
 
 
+// function display(ll) {
+//   console.log(ll);
+// }
+
+// function displayValues(ll) {
+//   let currentNode = ll.head;
+//   while(currentNode !== null) {
+//     console.log(currentNode.value);
+//     currentNode = currentNode.next;
+//   }
+// }
+
+// displayValues(newList);
+
+// function size(ll) {
+//   let count = 0;
+//   let currNode = ll.head;
+//   while (currNode !== null) {
+//     currNode = currNode.next;
+//     count++;
+//   }
+//   return count;
+// }
+
+// console.log(size(newList));
+
+
+
+// function isEmpty(ll) {
+//   if(ll.head === null) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(isEmpty(testList));
+
+// function findPrevious(item, ll) {
+//   let currNode = ll.head;
+//   while(currNode.next.value !== item) {
+//     currNode = currNode.next;
+//   }
+//   return currNode;
+// }
+
+// console.log(findPrevious('Tauhida', newList));
+
+// function findLast(ll) {
+//   let currNode = ll.head;
+//   while(currNode.next !== null) {
+//     currNode = currNode.next;
+//   }
+//   return currNode;
+// }
+
+// console.log(findLast(newList));
